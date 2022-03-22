04d6d0308347e320ac8952dd8eef7dc7c542622e
Marzio95 <marziodellarocca14@gmail.com>
2022-03-22 19:36:39 +0100

    termino il layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,47 +1,62 @@
 <template>
   <div class="contenitore">
-    <div class="row">
-        <div class="col_left">
-            <div>
-                <ul>
-                    <h3>DC COMICS</h3>
-                    <li v-for="(element, index) in arrayFooter1" :key="index">
-                    <a href="#">{{ element.text }}</a>
-                    </li>
-                </ul>
+        <div class="row">
+            <div class="col_left">
+                <div>
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li v-for="(element, index) in arrayFooter1" :key="index">
+                        <a href="#">{{ element.text }}</a>
+                        </li>
+                    </ul>
 
-                <ul>
-                    <h3>SHOP</h3>
-                    <li v-for="(element, index) in arrayFooter2" :key="index">
-                    <a href="#">{{ element.text }}</a>
-                    </li>
-                </ul>
+                    <ul>
+                        <h3>SHOP</h3>
+                        <li v-for="(element, index) in arrayFooter2" :key="index">
+                        <a href="#">{{ element.text }}</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <ul>
+                        <h3>DC</h3>
+                        <li v-for="(element, index) in arrayFooter3" :key="index">
+                        <a href="#">{{ element.text }}</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <ul>
+                        <h3>SITES</h3>
+                        <li v-for="(element, index) in arrayFooter4" :key="index">
+                        <a href="#">{{ element.text }}</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
-            <div>
-                <ul>
-                    <h3>DC</h3>
-                    <li v-for="(element, index) in arrayFooter3" :key="index">
-                    <a href="#">{{ element.text }}</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <ul>
-                    <h3>SITES</h3>
-                    <li v-for="(element, index) in arrayFooter4" :key="index">
-                    <a href="#">{{ element.text }}</a>
-                    </li>
-                </ul>
+            
+            <div class="col_right">
+                <img src="../assets/img/dc-logo-bg.png" alt="" />
             </div>
         </div>
-        
-        <div class="col_right">
-            <img src="../assets/img/dc-logo-bg.png" alt="" />
+        <div class="edit">
+            <div class="col_edit_left">
+                <button>SIGN-UP NOW!</button>
+            </div>
+
+            <div class="col_edit_right">
+                <div class="follow">FOLLOW US</div>
+                <img src="../assets/img/footer-facebook.png" alt="">
+                <img src="../assets/img/footer-twitter.png" alt="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+                <img src="../assets/img/footer-pinterest.png" alt="">
+                <img src="../assets/img/footer-periscope.png" alt="">
+            </div>
+
         </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -167,5 +182,34 @@ a {
   text-decoration: none;
   color: gray;
   font-weight: bold;
+}
+.edit{
+    background-color: #303030;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 6rem;
+}
+.col_edit_right{
+    display: flex;
+    align-items: center;
+}
+img{
+    padding: 0 0.4rem;
+}
+.follow{
+    color: #0282f9;
+    padding-right: 1.4rem;
+    font-weight: bold;
+    font-size: 1.4rem;
+}
+button{
+    border: 2px solid #0282f9;
+    background-color: transparent;
+    color: white;
+    font-weight: bold;
+    font-size: 1.4rem;
+    padding: 1rem;
+    cursor: pointer;
 }
 </style>
